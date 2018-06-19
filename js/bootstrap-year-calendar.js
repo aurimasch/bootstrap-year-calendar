09c5eaf306d254b717f1ea4d8675fb18d2d3c41a
Rokas Ulickas <ulickas.rokas@gmail.com>
2018-06-19 15:53:42 +0300

    a

--- js/bootstrap-year-calendar.js
+++ js/bootstrap-year-calendar.js
@@ -584,7 +584,7 @@
                 cells.bind('touchmove', function (e) {
                 	console.log(e);
                     if (_this._mouseDown) {
-                        var currentDate = _this._getDate(e.targetTouches[0]);
+                        var currentDate = _this._getDate(e.targetTouches[0].target);
 
                         console.log(!_this.options.allowOverlap);
 
